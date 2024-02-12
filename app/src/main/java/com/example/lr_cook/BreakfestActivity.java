5205d0388ae15a359963692bab50aa53ae896d06
soni1541 <kurbatovasoni5@gmail.com>
2024-02-12 22:26:48 +0500

    Завтраки все готовы

--- app/src/main/java/com/example/lr_cook/BreakfestActivity.java
+++ app/src/main/java/com/example/lr_cook/BreakfestActivity.java
@@ -2,7 +2,9 @@ package com.example.lr_cook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BreakfestActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class BreakfestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfest);
+    }
+
+    public void show_breakfest1(View view)
+    {
+        Intent intent = new Intent(BreakfestActivity.this, Breakfest1Activity.class);
+        startActivity(intent);
+    }
+    public void show_breakfest2(View view)
+    {
+        Intent intent = new Intent(BreakfestActivity.this, Breakfest2Activity.class);
+        startActivity(intent);
+    }
+    public void show_breakfest3(View view)
+    {
+        Intent intent = new Intent(BreakfestActivity.this, Breakfest3Activity.class);
+        startActivity(intent);
     }
 }
