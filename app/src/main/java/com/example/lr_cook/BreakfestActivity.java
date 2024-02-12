59ca5e4982f035e120b8e0804afed9f33fef1d13
soni1541 <kurbatovasoni5@gmail.com>
2024-02-12 17:43:17 +0500

    Начаты завтраки

---
+++ app/src/main/java/com/example/lr_cook/BreakfestActivity.java
@@ -0,0 +1,14 @@
+package com.example.lr_cook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BreakfestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breakfest);
+    }
+}
