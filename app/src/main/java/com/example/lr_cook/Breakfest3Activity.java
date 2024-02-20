08f8b3ecf62538e0981d9b5dc620557982f71672
soni1541 <kurbatovasoni5@gmail.com>
2024-02-20 14:14:03 +0500

    Сделан таймер

--- app/src/main/java/com/example/lr_cook/Breakfest3Activity.java
+++ app/src/main/java/com/example/lr_cook/Breakfest3Activity.java
@@ -2,13 +2,64 @@ package com.example.lr_cook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class Breakfest3Activity extends AppCompatActivity {
+
+    Button btn_start;
+
+    long minutes;
+
+    private TextView timer_text_view;
+
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfest3);
+
+        btn_start = findViewById(R.id.button_start_breakfest3);
+        timer_text_view = findViewById(R.id.textView_timer_breakfest3);
+
+        minutes = 120000;
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(timer!=null)
+                {
+                    timer.cancel();
+                }
+                btn_start.setText("Сброс");
+                btn_start.setBackgroundColor(Color.BLACK);
+
+                timer = new CountDownTimer(minutes, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        NumberFormat f = new DecimalFormat("00");
+                        long sec = l/1000;
+                        long min = sec/60;
+                        sec = sec%60;
+                        String time = f.format(min) + ":" + f.format(sec);
+                        timer_text_view.setText(time);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        btn_start.setText("СТАРТ");
+                        btn_start.setBackgroundColor(Color.parseColor("#FF9800"));
+                    }
+                };
+                timer.start();
+            }
+        });
     }
 }
